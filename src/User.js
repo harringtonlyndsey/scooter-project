8235d4ddbe44d796b8f0eb94bf6d78124bf421d2
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-10-27 11:32:21 +0100

    changes made to Scooter

--- src/User.js
+++ src/User.js
@@ -1,5 +1,11 @@
 class User {
-  // User code here
+  constructor(username, password, age) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+  }
+  login(password) {}
+  logout() {}
 }
 
-module.exports = User
+module.exports = User;
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,27 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
+const Scooter = require("../src/Scooter");
+const User = require("../src/User");
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
+describe("scooter object", () => {
+  test("Scooter class should create Scooter instance", () => {
     const scooter = new Scooter();
     expect(scooter).toBeInstanceOf(Scooter);
   });
-})
+});
 
 //Method tests
-describe('scooter methods', () => {
+describe("scooter methods", () => {
   // tests here!
 
   //rent method
-  
 
   //dock method
 
   //requestRepair method
 
-  //charge method
-
-})
+  test("charge", async () => {
+    const scooter = new Scooter();
+    await scooter.charge(); // we need to wait for the charge!
+    expect(newScooter.charge).toBe(100);
+  });
+});
